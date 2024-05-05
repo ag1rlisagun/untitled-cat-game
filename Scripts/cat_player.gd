ea3da4bf57314a51489d1c9fa5f2593dcf757d8a
ag1rlisagun <aaliyahwusu@gmail.com>
2024-05-05 00:51:33 -0600

    new idle animation and camera fix

--- Scripts/cat_player.gd
+++ Scripts/cat_player.gd
@@ -8,6 +8,7 @@ extends CharacterBody2D
 
 var selected = false
 var crouching = false
+var has_idled = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -26,6 +27,8 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 		selected = true
 		
 func crouch():
+	if !is_on_floor():
+		return
 	if crouching:
 		return
 	crouching = true
@@ -47,11 +50,15 @@ func _physics_process(delta):
 		# GRAVITY
 		if not is_on_floor():
 			velocity.y += gravity * delta
+			has_idled = false
+			$Timer.start()
 
 		# JUMP
 		# can add double jump/wall jump/etc.
 		if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
+			velocity.y = JUMP_VELOCITY 
+			has_idled = false
+			$Timer.start()
 
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
@@ -73,25 +80,32 @@ func _physics_process(delta):
 			crouch()
 		elif Input.is_action_just_released("crouch"):
 			stand()
+		
+		if Input.is_action_just_released("move_left") or Input.is_action_just_released("move_right") or Input.is_action_just_released("crouch"):
+			has_idled = false
+			$Timer.start()
 			
 		# Play Animations
 		if is_on_floor():
 			if direction == 0:
-				animated_sprite.play("side idle")
+				if has_idled:
+					animated_sprite.play("idle")
+				elif !has_idled:
+					animated_sprite.play("side idle")
 				if crouching:
 					animated_sprite.play("crouch")
 				if Input.is_action_just_pressed("jump"):
 					animated_sprite.play("jump")
-			else: 
+			else:
 				if !crouching:
-					if Input.is_action_just_pressed("jump"):
-						animated_sprite.play("jump")
-					elif Input.is_action_pressed("sprint"):
+					if Input.is_action_pressed("sprint"):
 						animated_sprite.play("sprint")
 					else:
 						animated_sprite.play("move")
 				else:
 					animated_sprite.play("crawl")
+				if Input.is_action_just_pressed("jump"):
+					animated_sprite.play("jump")
 		else:
 			if velocity.y > 0:
 				animated_sprite.play("fall")  # Play falling animation when not on floor
@@ -106,3 +120,7 @@ func _physics_process(delta):
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 		move_and_slide()
+
+
+func _on_timer_timeout():
+	has_idled = true
